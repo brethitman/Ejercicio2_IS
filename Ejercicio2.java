4029e87904fb8c423f5c1e002e4bc8840c2ec867
fernando <luisquena7@gmail.com>
2025-09-22 12:36:05 -0400

    operacion minimo

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,6 @@
 public class Ejercicio2 {
-   
+
+    // Método para obtener el número mayor
     void MAXIMIZA_josue() {
         int a = 10;
         int b = 25;
@@ -15,9 +16,26 @@ public class Ejercicio2 {
         System.out.println("El número mayor es: " + maximo);
     }
 
-    
+    // Método para obtener el número menor
+    void minimo_fernando() {
+        int a = 8;
+        int b = 19;
+
+        int minimo;
+
+        if (a < b) {
+            minimo = a;
+        } else {
+            minimo = b;
+        }
+
+        System.out.println("El número menor es: " + minimo);
+    }
+
     public static void main(String[] args) {
         Ejercicio2 ejercicio = new Ejercicio2();
-        ejercicio.MAXIMIZA_josue(); 
+        ejercicio.MAXIMIZA_josue();
+        ejercicio.minimo_fernando(); 
     }
 }
+
