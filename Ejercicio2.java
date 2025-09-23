2be1079c1c5ed3af79ddc26cffd319e5f3aa2bfc
brethitman <yosueanverson12@gmail.com>
2025-09-22 22:30:35 -0400

    Actualización en Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -17,7 +17,7 @@ public class Ejercicio2 {
 
     //siguiente metodo aqui csmd
 
-
+    
     public static void main(String[] args) {
         Ejercicio2 ejercicio = new Ejercicio2();
         ejercicio.MAXIMIZA_josue(); 
